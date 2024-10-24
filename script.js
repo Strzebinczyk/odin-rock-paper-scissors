dbbb71a15e6d5afd7e16a1f785693963d9c73a84
Strzebinczyk <kar.strzebinczyk@gmail.com>
2024-10-24 13:45:15 +0200

    Fix style reference, add computer choice

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
-console.log("Hello World!");
+
+function getComputerChoice() {
+  let random = Math.random(); 
+  if (random <= 0.33) {
+    return "Rock";
+  } else if (random <= 0.66) {
+    return "Paper";
+  } else if (random <= 1) {
+    return "Scissors";
+  } else {
+    return "Unexpected number";
+  }
+};
+
+let computerChoice = getComputerChoice();
