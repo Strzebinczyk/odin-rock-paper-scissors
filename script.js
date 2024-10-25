5d9b8f8dab5144f10207e43051ecf5fccef58a11
Strzebinczyk <kar.strzebinczyk@gmail.com>
2024-10-25 10:32:56 +0200

    Add logic to play a single round

--- script.js
+++ script.js
@@ -11,24 +11,63 @@ function getComputerChoice() {
   } else {
     result = "Unexpected number";
   }
-  document.querySelector(".computerOutput").textContent = result
-  return result
+  document.querySelector(".computerOutput").textContent = result;
+  return result;
 };
 
 function getUserChoice() {
   let result
-  let selectElement = document.querySelector("#dropdown-menu");
+  let selectElement = document.querySelector("#dropdownMenu");
   result = selectElement.value;
   document.querySelector(".userOutput").textContent = result;
   return result;
 }
 
-function getChoices() {
+function playRound(userChoice, computerChoice) {
+  document.querySelector(".roundNumber").textContent = roundNumber;
+  roundNumber += 1;
+  if ((userChoice == "Rock" && computerChoice == "Scissors") || (userChoice == "Paper" && computerChoice == "Rock") || (userChoice == "Scissors" && computerChoice == "Paper")) {
+    return "win";
+  } else if ((userChoice == "Rock" && computerChoice == "Paper") || (userChoice == "Paper" && computerChoice == "Scissors") || (userChoice == "Scissors" && computerChoice == "Rock")) {
+    return "lose";
+  } else {
+    return "tie"
+  }
+}
+
+function adjustScore(result) {
+  document.querySelector(".result").textContent = result;
+
+  switch (result) {
+    case "win":
+      userScore += 1;
+      document.querySelector(".userScore").textContent = userScore;
+      document.querySelector(".computerScore").textContent = computerScore;
+      break;
+    case "lose":
+      computerScore += 1;
+      document.querySelector(".userScore").textContent = userScore;
+      document.querySelector(".computerScore").textContent = computerScore;
+      break;
+    case "tie":
+      document.querySelector(".userScore").textContent = userScore;
+      document.querySelector(".computerScore").textContent = computerScore;
+      break;
+    default:
+      console.log("Something unexpected happened in adjustScore")
+  }
+}
+
+function play() {
   userChoice = getUserChoice();
   computerChoice = getComputerChoice();
+  adjustScore(playRound(userChoice, computerChoice));
 }
+
+
 
 let userChoice;
 let computerChoice;
 let userScore = 0;
 let computerScore = 0;
+let roundNumber = 1;
